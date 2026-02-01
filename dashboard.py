eaf2a2c57adf64d554adcf5bea347fac7df6385c
Damar Syarafi Ramadhan <damarsyarafi.rmdhn@gmail.com>
2026-02-01 22:15:50 +0700

    Refactor data loading in dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-def load_data():
-  df = pd.read_csv("amazon.csv")
-  df.head(5)
+df = pd.read_csv('amazon.csv')
 
 st.title("Mesin Sistem Rekomendasi Produk Platform E-commerce Amazon")
 
 with st.expander('Data'):
   st.write("Raw Data")
-  load_data()
+  df
