03aa7aacff33fbd42261465be64c170c1b8e9fd8
Damar Syarafi Ramadhan <damarsyarafi.rmdhn@gmail.com>
2026-02-01 22:18:24 +0700

    Update raw data display title in dashboard

--- dashboard.py
+++ dashboard.py
@@ -6,5 +6,5 @@ df = pd.read_csv('amazon.csv')
 st.title("Mesin Sistem Rekomendasi Produk Platform E-commerce Amazon")
 
 with st.expander('Data'):
-  st.write("Raw Data")
-  df
+  st.write("Amazon items Raw Data")
+  amazon_items = df[["product_name", "category", "actual_price","discount_percentage","rating","rating_count"
