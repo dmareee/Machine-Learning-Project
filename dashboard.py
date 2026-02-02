72abb82844c26a170f8868c5e926c6d0b7a9806d
Damar Syarafi Ramadhan <damarsyarafi.rmdhn@gmail.com>
2026-02-02 01:03:02 +0000

    Add data info

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,32 @@
 import streamlit as st
+import pickle
 import pandas as pd
+import plotly.express as px
+import numpy as np
 
 df = pd.read_csv('amazon.csv')
 
 st.title("Mesin Sistem Rekomendasi Produk Platform E-commerce Amazon")
 
 with st.expander('Data'):
+  st.info("""Dataset Amazon Sales yang diambil dari Kaggle berisi informasi lengkap terkait produk dan ulasan pengguna, dengan struktur variabel sebagai berikut:
+          - product_id: ID unik produk
+          - product_name: Nama produk
+          - category: Kategori produk
+          - discounted_price: Harga produk setelah diskon
+          - actual_price: Harga asli produk sebelum diskon
+          - discount_percentage: Persentase diskon produk
+          - rating: Rating rata-rata produk
+          - rating_count: Jumlah pengguna yang memberikan rating
+          - about_product: Deskripsi produk
+          - user_id: ID pengguna yang memberikan review
+          - user_name: Nama pengguna yang memberikan review
+          - review_id: ID review pengguna
+          - review_title: Judul singkat review
+          - review_content: Isi review panjang
+          - img_link: Link gambar produk
+          - product_link: Link resmi produk di website Amazon
+          """)
   st.write("Amazon items Raw Data")
   amazon_items = df[["product_name", "category", "actual_price","discount_percentage","rating","rating_count"]]
   amazon_items
