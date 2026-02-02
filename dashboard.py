71200538f8845fd206d168665f98d0cdc63d6cba
Damar Syarafi Ramadhan <damarsyarafi.rmdhn@gmail.com>
2026-02-02 03:12:01 +0000

    change view avg disc colors

--- dashboard.py
+++ dashboard.py
@@ -48,7 +48,7 @@ def view_average_discount(data):
     category_disc= category_disc.sort_values(by='discounted_price', ascending=False)
     category_disc
     st.header(f'Rata-rata Harga Diskon Setiap Kategori Produk')
-    st.bar_chart(category_disc, x='category', y='discounted_price', color=["#F54927", "#6CF527", "#27D3F5", "#B027F5", "#F5E027", "#F58A27"])
+    st.bar_chart(category_disc, x='category', y='discounted_price', color='category')
 view_average_discount(data)
 def view_recommendation(model, product_name, num_recommendations):
     try:
